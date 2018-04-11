ad8b50d7fb34afdeb5ee06bc6f59439f610cdddf
Mitchell LeMoine <whitefang573@gmail.com>
2018-04-10 19:32:08 -0700

    add dependencies to package add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var autoprefixer = require('autoprefixer'),
+    gulp = require('gulp'),
+    newer = require('gulp-newer'),
+    postcss = require('gulp-postcss'),
+    sass = require('gulp-sass');
+
+gulp.task('default', function () {
+    console.log('Hello, world!');
+});
